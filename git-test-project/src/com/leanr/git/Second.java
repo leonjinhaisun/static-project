9d95203b25a6af0a52fea66c1840da3912c00d8d
leon <870116654@qq.com>
2019-10-25 22:25:02 +0800

    Second类打印横线

--- git-test-project/src/com/leanr/git/Second.java
+++ git-test-project/src/com/leanr/git/Second.java
@@ -3,5 +3,6 @@ package com.leanr.git;
 public class Second {
     public void test(){
         System.out.println("hello second!");
+        System.out.println("--------------------------------------");
     }
 }
